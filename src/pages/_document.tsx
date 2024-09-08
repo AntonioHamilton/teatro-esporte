6f7e0553ed98be1deb298c5d6d27661fe7a610ac
Antonio Hamilton <antoniohamilton.s.freitas@gmail.com>
2024-09-08 17:28:31 -0300

    chore: initial next configs

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,13 +1,16 @@
-import { Html, Head, Main, NextScript } from "next/document";
+import StyledComponentsRegistry from '@/lib/registry';
+import { Html, Head, Main, NextScript } from 'next/document';
 
 export default function Document() {
-  return (
-    <Html lang="en">
-      <Head />
-      <body>
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
-  );
+	return (
+		<Html lang="en">
+			<Head />
+			<body>
+				<StyledComponentsRegistry>
+					<Main />
+					<NextScript />
+				</StyledComponentsRegistry>
+			</body>
+		</Html>
+	);
 }
---
+++ src/pages/test/index.tsx
@@ -0,0 +1,5 @@
+import { Container } from './index.styled';
+
+const TestPage = () => <Container>HELLO</Container>;
+
+export default TestPage;
